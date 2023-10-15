71c129b409893a1c06974c4477bd337dd1865396
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-10-15 08:58:09 -0400

    added prompts to index.js

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+const fs = require('fs');
 const inquirer = require("inquirer");
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 const {Circle, Square, Triangle} = require("./lib/shapes");
+
+inquirer
+    .prompt([
+        {
+            type: 'maxlength-input',
+            message: 'What is your three letter logo?',
+            maxLength: 3,
+            name: 'text',
+        },
+        {
+            type: 'input',
+            message: 'Please select a text color.',
+            name: 'color',
+        },
+        {
+            type: 'list',
+            message: 'Please select a shape.',
+            choices: ['circle', 'triangle', 'square'],
+            name: 'shape',
+        },
+    ])
+    .then((response) =>
+        fs.writeFile(response.text.toLowerCase().split(" ").join("") + ".svg", `
+
+`, (err) =>
+            err ? console.error(err) : console.log('success!')
+        )
+    );
